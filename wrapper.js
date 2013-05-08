82d82b82cb05f7f394d503dfd03ab5dcdef15c96
AndoniC <andonicor@gmail.com>
2013-05-08 20:24:12 +0200

    MOD: estructura

---
+++ wrapper.js
@@ -0,0 +1,50 @@
+
+function init(server_list)
+
+{
+
+    //Modificar la pagina para que muestre la lista de servers como botones
+
+}
+
+function on_push_button_load_series_list(server)
+
+{
+
+ // parsear lista de series del servidor seleccionado
+
+ // modificar pagina para que muestre la lista
+
+}
+
+function on_push_button_load_season(serie)
+
+{
+
+ // parsear las temporadas de la serie
+
+ // modificar dom de pagina para que muestre la lista de temporadas
+
+}
+
+function on_push_button_load_chapters(season)
+
+{
+
+ // parsear los capitulos de la temporada
+
+ // modificar dom de pagina para que muestre la lista de capitulos
+
+}
+
+function play(chapter)
+
+{
+
+ // buscar y/o generar  el link
+
+ // modificar dom de pagina para que muestre el player con el link detectado
+
+}
+
+
